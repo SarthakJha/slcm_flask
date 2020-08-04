cdff27f341290a14cc02946619f4887b8112044e
Sarthak Jha <sarthakjha2001@gmail.com>
2020-08-04 23:50:07 +0530

    major changes

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    username = 'xxx'
-    password = 'xxxx'
+    username = '190905191'
+    password = '2001@Billion'
 
     chrome_options = Options()
     chrome_options.add_argument('headless')
@@ -62,9 +62,6 @@ def hello():
             index = all_td[0].text.strip()
             name = all_td[1].text.strip()
             link = all_td[2].find('a')['href']
-            # contentTitle = name
-            # print(index, " ", name)
-            # contentTitles.append(name)
             titleDict = {
                 "title": name,
                 "index": int(index),
@@ -81,94 +78,14 @@ def hello():
     })
 
 
-# Downloads specific pdf
+# sends specific pdf
 
-
-@app.route('/getpdf/<file_name>', methods=['POST'])
-def getPDF(file_name):
-    req_data = request.get_json()
-    requestedPage = req_data['page']
-    requestedIndex = req_data['index']
-
-    # username = 'xxx'
-    # password = 'xxxx'
-
-    # chrome_options = Options()
-    # chrome_options.add_argument('headless')
-    # # To Remove chrome from popping up, Uncomment the previous line
-    # chrome_options.add_argument('--disable-infobars')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--remote-debugging-port=9191')
-    # browser = webdriver.Chrome(
-    #     "/Users/srathakjha/Desktop/MTTN-SLCM-Notices/chromedriver")
-
-    # # Login
-    # browser.get('https://slcm.manipal.edu/')
-    # user_bar = browser.find_element_by_name('txtUserid')
-    # print(user_bar)
-    # pass_bar = browser.find_element_by_name('txtpassword')
-    # login_btn = browser.find_element_by_name('btnLogin')
-    # user_bar.send_keys(username)
-    # pass_bar.send_keys(password)
-    # login_btn.click()
-    # time.sleep(5)
-
-    # # Open Notices
-    # browser.get('https://slcm.manipal.edu/ImportantDocuments.aspx')
-    # page = 1
-    # contentTitleDict = []
-    # k = 0
-
-    # while(page < requestedPage + 1):
-    #     time.sleep(5)
-    #     table = browser.find_element_by_id('ContentPlaceHolder1_grvDocument')
-    #     tra = table.find_elements_by_tag_name('tr')
-    #     tra = tra[1:]
-    #     page_row = tra[len(tra)-1]
-    #     two_button = page_row.find_elements_by_tag_name('td')
-    #     tra = tra[:len(tra)-2]
-    #     nextl = two_button[page].find_element_by_tag_name('a')
-
-    #     if page == requestedPage:
-    #         print('requested page found')
-    #         table = browser.find_element_by_id(
-    #             'ContentPlaceHolder1_grvDocument')
-    #         tra = table.find_elements_by_tag_name('tr')
-    #         tra = tra[1:]
-    #         page_row = tra[len(tra)-1]
-    #         tra = tra[:len(tra)-2]
-    #         for i in tra:
-    #             k = k+1
-    #             downloadbtn = i.find_element_by_tag_name('a')
-    #             source = i.get_attribute('innerHTML')
-    #             tr = BeautifulSoup(source, 'html.parser')
-    #             all_td = tr.find_all('td')
-    #             index = all_td[0].text.strip()
-    #             name = all_td[1].text.strip()
-    #             link = all_td[2].find('a')['href']
-    #             titleDict = {
-    #                 "title": name,
-    #                 "index": index,
-    #                 "page": page
-    #             }
-    #             contentTitleDict.append(titleDict)
-    #             standardX = k+((page-1)*10)
-    #             if requestedIndex == standardX:
-    #                 print(requestedIndex == standardX)
-    #                 # downloadbtn.click()
-    #                 time.sleep(5)
-    #                 print('exiting loop')
-    #                 break
-    #         break
-
-    #     else:
-    #         page = page + 1
-    #         nextl.click()
-    #         print('next page hehe')
-    #         continue
-
-    pdfRoute = "/Users/srathakjha/Desktop/slcm_MTTN/notices/" + \
-        str(requestedPage)
+@app.route('/getpdf/<int:page>/<file_name>', methods=['GET'])
+def getPDF(file_name, page):
+    print(page)
+    pdfRoute = "/Users/srathakjha/Desktop/flask_slcm_MTTN/notices/" + \
+        str(page)
+    print(pdfRoute)
     try:
         print("sending pdf")
         return send_from_directory(pdfRoute, filename=file_name, as_attachment=False)
@@ -178,4 +95,4 @@ def getPDF(file_name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
